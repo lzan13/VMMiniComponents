20aa9ce019988b766d04402fb77f2e1e76ba1930
liulizheng <liulizheng@weidian.com>
2019-01-15 18:54:23 +0800

    删除无用的代码

--- utils/vmutil.js
+++ utils/vmutil.js
@@ -42,24 +42,10 @@ const formatStr = function() {
 }
 
 
-function subString(target) {
-  if (target && target.length > 12) {
-    target = target.replace(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/g, "");
-    if (!target) {
-      return '匿名';
-    } else {
-      return target.slice(0, 12) + '...';
-    }
-  }
-  return target;
-}
-
-
 /**
  * 将方法暴露出去
  */
 module.exports = {
   formatTime: formatTime,
-  formatStr: formatStr,
-  subString: subString
+  formatStr: formatStr
 }
